d0111b0c7902ef3d0ca5b844a3531207d0d60d28
Nakhon-Start <56624645+Nakhon-Start@users.noreply.github.com>
2020-08-26 23:32:53 +0700

    Success

--- app/src/main/java/com/example/firstapp/ContactFragment.kt
+++ app/src/main/java/com/example/firstapp/ContactFragment.kt
@@ -1,14 +1,48 @@
 package com.example.firstapp
 
 import android.os.Bundle
+import android.view.*
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.firstapp.database.ContactDatabase
+import com.example.firstapp.databinding.FragmentContactBinding
 
 class ContactFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_contact, container, false)
+        var binding = DataBindingUtil.inflate<FragmentContactBinding>(
+            inflater,
+            R.layout.fragment_contact,
+            container,
+            false
+        )
+
+        setHasOptionsMenu(true)
+
+        val application = requireNotNull(this.activity).application
+        val dataSource = ContactDatabase.getInstance(application).contactDAo
+        val viewModelFactory = ContactViewModelFactory(dataSource, binding, application)
+        val contactViewModel =
+            ViewModelProvider(
+                this, viewModelFactory
+            ).get(ContactViewModel::class.java)
+        binding.contactViewModel = contactViewModel
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.navdrawer_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(
+            item,
+            view!!.findNavController()
+        ) || super.onOptionsItemSelected(item)
     }
 }
